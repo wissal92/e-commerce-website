8f47e23e558ad3db14defea1608b2808e7b52764
wissal <wissal.ouarsafi@gmail.com>
2020-01-21 19:58:49 -0800

    Create edit product page with the get and post routes

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -6,6 +6,7 @@ const {handleErrors, requireAuth} = require('./middlwares');
 const productsRepo = require('../../repositories/products');
 const productsTemplate = require('../../views/admin/products/new');
 const productsIndexTemplate = require('../../views/admin/products/index');
+const productEditTemplate = require('../../views/admin/products/edit');
 const {requireTitle, requirePrice} = require('./validators');
 
 const router = express.Router();
@@ -27,6 +28,33 @@ router.post('/admin/products/new', requireAuth, upload.single('image'), [require
     await productsRepo.create({title, price, image});
 
    res.redirect('/admin/products');
+});
+
+router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
+    const product = await productsRepo.getOne(req.params.id);
+
+    if (!product) {
+        return res.send('Product not found');
+    }
+
+    res.send(productEditTemplate({product}));
+});
+
+router.post('/admin/products/:id/edit', requireAuth,upload.single('image'),[requireTitle, requirePrice], handleErrors(productEditTemplate), async (req, res) => {
+    const changes = req.body;
+
+    if (req.file) {
+      changes.image = req.file.buffer.toString('base64');
+    }
+
+    try {
+      await productsRepo.update(req.params.id, changes);
+    } catch (err) {
+      return res.send('Could not find item');
+    }
+
+    res.redirect('/admin/products');
+
 });
 
 module.exports = router;
---
+++ views/admin/products/edit.js
@@ -0,0 +1,35 @@
+const layout = require('../layout');
+const {getError} = require('../../helpers');
+
+module.exports = ({product, errors }) => {
+  return layout({
+    content: `
+      <div class="columns is-centered">
+        <div class="column is-half">
+          <h1 class="subtitle">Edit a Product</h1>
+
+          <form method="POST" enctype="multipart/form-data">
+            <div class="field">
+              <label class="label">Title</label>
+              <input class="input" placeholder="Title" name="title" value='${product.title}'>
+              <p class="help is-danger">${getError(errors, 'title')}</p>
+            </div>
+            
+            <div class="field">
+              <label class="label">Price</label>
+              <input class="input" placeholder="Price" name="price" value='${product.price}'>
+              <p class="help is-danger">${getError(errors, 'price')}</p>
+            </div>
+            
+            <div class="field">
+              <label class="label">Image</label>            
+              <input type="file" name="image"/>
+            </div>
+            <br />
+            <button class="button is-primary">Create</button>
+          </form>
+        </div>
+      </div>
+    `
+  });
+};
